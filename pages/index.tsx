1ca09193f62a04941f587fb1e431adabcd69ab04
Katleho-codes <Katlehomabala3@gmail.com>
2022-11-14 00:32:57 +0200

    "Changed homepage"

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,37 @@
 import Link from "next/link";
-
+import Image from "next/image";
+import man1 from "../public/images/models/m1.jpg";
+import man2 from "../public/images/models/m2.jpg";
+import woman1 from "../public/images/models/w1.jpg";
+import woman2 from "../public/images/models/w2.jpg";
 const Home = () => {
   return (
     <>
       <main className="home">
-        <div className="hero_image">
+        <div className="container">
           <section className="homepage_text">
             <h1>Become a star</h1>
             <h4>Modelling agency</h4>
             <Link href="/about" className="btn home_cta">
               Who we are
             </Link>
+          </section>
+          <section className="images_home">
+            <article className="col">
+              <Image src={man1} alt="" />
+            </article>
+            <article className="col">
+              <Image src={woman1} alt="" />
+            </article>
+            <article className="col">
+              <Image src={man2} alt="" />
+            </article>
+            <article className="col">
+              <Image src={woman2} alt="" />
+            </article>
+          </section>
+          <section className="who_we_are page_title ">
+            <h1>Who we are</h1>
           </section>
         </div>
       </main>
